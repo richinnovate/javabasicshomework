164e63753c4d6e514054e6f8b0e667d049e13ba8
Lloyd Richardson <richinnovate@gmail.com>
2016-07-25 16:35:34 -0400

    Homework

---
+++ day-1.js
@@ -0,0 +1,45 @@
+// 1. Assign the message "Hello, World!" to a variable.
+let n = "Hello World";
+
+// 2. Assign a different string to a different variable.
+let a = "What's up";
+
+// 3. Assign a number to a variable.
+let b = 15;
+
+// 4. Use string concatenation to display the number from #3 in a string.
+console.log("What's on line three: " + b)
+
+// 5. Make an array of at least four of your favorite movies or books or bands.
+let favmovies = ["Harry Potter", "Shawshank Redemption", "GoodWill Hunting", "The Lord Of The Rings"]
+console.log (favmovies)
+
+// 6. Make a object of information about yourself with at least four properties.
+const glossary = {
+Born: "Miami",
+Weight: "200",
+Shoe: "11",
+kids: "4",
+}
+console.log (glossary)
+// 7. Make an array of objects containing more information about your favorite movies. The objects should have at least three properties.
+
+let favmoviesarray = [
+  { movie: "Harry Potter",
+    genre: "Fantasy Adventure",
+    year:  "2003"},
+
+  { movie: "Shawshank Redemption",
+    genre: "Drama",
+    year: "1999"},
+
+  { movie: "GoodWill Hunting",
+    genre: "Drama",
+    year: "2000"},
+
+  { movie: "The Lord of The Rings",
+    genre: "Fantasy Adventure",
+    year: "2004"}
+]
+console.log (favmoviesarray)
+// 8. Use `for` to loop through the answer from exercise #7 and print only one property from the object. i.e., given `{ title: "Gone with the Wind" }` you print "Gone with the Wind".
